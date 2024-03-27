0c360225c777aa352921964c85ca5d8e210b3737
CJ-Presley <144433536+CJ-Presley@users.noreply.github.com>
2024-03-27 14:05:04 +0000

    added logo, paths for pages and added logo as icon on index

---
+++ FrontEnd/RZA-DigitalSolution/src/constants/paths.ts
@@ -0,0 +1,25 @@
+const INDEX_PATH = "/";
+const CONTACTS_PATH = "/contacts";
+const ABOUT_PATH = "/about";
+const STAFF_DASHBOARD_PATH = "/PqRsTuV12345678";
+const CUSTOMER_DASHBOARD_PATH = "/Ysh£2Jknd242Opw";
+const TICKET_PATH = "/tickets";
+const MEMBER_PATH = "/memberships";
+const ROOM_PATH = "/rooms";
+const CHECKOUT_PATH = "/XyZ9876ABcd3210EfGh";
+const STAFF_SIGNUP_PATH = "Y4p4fpZFUSkcbESGW23x";
+const NOT_FOUND_PATH = "/*";
+
+export {
+  CHECKOUT_PATH,
+  INDEX_PATH,
+  CONTACTS_PATH,
+  ABOUT_PATH,
+  STAFF_DASHBOARD_PATH,
+  NOT_FOUND_PATH,
+  STAFF_SIGNUP_PATH,
+  CUSTOMER_DASHBOARD_PATH,
+  ROOM_PATH,
+  MEMBER_PATH,
+  TICKET_PATH,
+};
