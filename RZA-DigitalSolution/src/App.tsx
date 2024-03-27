b91adec446bdca7012dead8fd2e14a448708f4fd
CJ-Presley <144433536+CJ-Presley@users.noreply.github.com>
2024-03-27 13:14:35 +0000

    initial commit - backend

---
+++ RZA-DigitalSolution/src/App.tsx
@@ -0,0 +1,9 @@
+import "./App.css";
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+function App() {
+  return <></>;
+}
+
+export default App;
