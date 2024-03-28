86fdef646be267ae3ff0cdfa621aa9451a18ef48
CJ-Presley <144433536+CJ-Presley@users.noreply.github.com>
2024-03-28 13:47:03 +0000

    Login Form Created and Edited

--- FrontEnd/RZA-DigitalSolution/src/components/LoginModal.tsx
+++ FrontEnd/RZA-DigitalSolution/src/components/LoginModal.tsx
@@ -1,37 +1,44 @@
-import { useState } from "react";
-import Button from "react-bootstrap/Button";
-import Modal from "react-bootstrap/Modal";
 import { User } from "@carbon/react/icons";
-import { Container } from "react-bootstrap";
+import { useState } from "react";
+import { Button, Col, Modal, Row } from "react-bootstrap";
+import {
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBCard,
+  MDBCardBody,
+  MDBInput,
+  MDBBtn,
+  MDBIcon,
+} from "mdb-react-ui-kit";
+import LoginForm from "./LoginForm";
 
 function LoginModal() {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-
   return (
     <>
       <Button variant="tertiary" onClick={handleShow}>
-        <User className="mt-0" size="33" />
+        <User size="33" />
       </Button>
 
-      <Modal data-bs-theme="dark" show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
+      <Modal data-bs-theme="dark" show={show} onHide={handleClose} centered>
+        <LoginForm />
+        {/* 
           <Modal.Title>Modal heading</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+        <Modal.Body>
+          <LoginForm />
+        </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
+        </Modal.Footer> */}
       </Modal>
     </>
   );
 }
-
 export default LoginModal;
