4c39b3c46bb633f3111a555d0a1799cb908bcf66
CJ-Presley <144433536+CJ-Presley@users.noreply.github.com>
2024-03-28 15:00:50 +0000

    Added Backend Python Files

---
+++ BackEnd/Python/RZAFunctions.py
@@ -0,0 +1,70 @@
+# Import
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import cryptography
+from bcrypt import checkpw, gensalt, hashpw
+
+# import LoginFunctions as LF
+import sqlite3
+from sqlite3 import Error
+import re
+import datetime
+from dateutil.parser import parse
+
+# -------------------------- #
+
+# Functions
+
+
+def PasswordCheck(password):
+    passwordPattern = (
+        r"^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])([a-zA-Z0-9@#$%^_&-+=]+){5,16}$"
+    )
+    if re.match(passwordPattern, password) != None:
+        return True
+    else:
+        return False
+
+
+def UsernameCheck(username):
+    if len(username) < 5:
+        return True
+    elif len(username) > 16:
+        return True
+    elif not username.isalnum():
+        return True
+    else:
+        return False
+
+
+def IsDate(date, fuzzy=False):
+    try:
+        parse(date, fuzzy=fuzzy)
+        return True
+    except ValueError:
+        return False
+
+
+def DateCheck(date):
+    if IsDate(date, fuzzy=False):
+        date_format = "%Y-%m-%d"
+        try:
+            dateObject = datetime.datetime.strptime(date, date_format)
+            return True
+        except:
+            return False
+    else:
+        return False
+
+
+def EmailCheck(email):
+    regex = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b"
+    # pass the regular expression
+    # and the string into the fullmatch() method
+    if re.fullmatch(regex, email):
+        return True
+    else:
+        return False
+
+
+# -------------------------- #
---
+++ BackEnd/Python/RZAServerEnviroment.py
@@ -0,0 +1,184 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import cryptography
+from bcrypt import checkpw, gensalt, hashpw
+import RZAFunctions as LF
+import sqlite3
+from dateutil.parser import parse
+from sqlite3 import Error
+import re
+import datetime
+
+# app = Flask function - name as variable fed into function
+app = Flask(__name__)
+# Cross Origin Resourse Sharing - manually define who can send information to server
+# CORS function from flask_cors : (see line 10), resource = {regex"anywhere/thing" : {(data can be sent from anywhere)}}
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+
+# login
+@app.route("/login", methods=["POST"])
+def LoginDetails():
+    logins = {}
+    print("Request Recieved")
+    with sqlite3.connect(r"Lorem-Ipsum/Database/RZA-Data.db") as conn:
+        print("Connection Established")
+        username = request.json.get("username")
+        if LF.UsernameCheck(username):
+            return jsonify(
+                {
+                    "success": False,
+                    "message": "Username must be 5 - 16 characters longs and alphanumeric",
+                }
+            )
+        password = request.json.get("password")
+        print(password)
+        if LF.PasswordCheck(password):
+            password = password.encode("utf-8")
+        else:
+            return jsonify(
+                {
+                    "success": False,
+                    "message": "Password must be between 5 and 16 characters and have atleast 1 uppercase character, 1 lowercase character and 1 number",
+                }
+            )
+        try:
+            cu = conn.cursor()
+            print("Cursor Created")
+            query = """Select Users.Password From Users Where Users.Username = ?"""
+            cu.execute(query, (username,))
+            results = cu.fetchall()
+            print(results)
+            for i in results:
+                logins[i[3]] = i[5]
+                role = i[6]
+                print(role)
+                print(logins)
+                print(logins[username])
+            if checkpw(password, logins[username]):
+                return jsonify(
+                    {"success": True, "message": "Login Successful", "role": role}
+                )
+            else:
+                return jsonify({"success": False, "message": "Incorrect Login Details"})
+        except Exception as e:
+            print(e)
+            return jsonify({"success": False, "message": "Incorrect Login Details"})
+
+
+# sign up - customer
+@app.route("/signup", methods=["POST"])
+def SignupDetails():
+    print("Request Recieved")
+    with sqlite3.connect(r"Lorem-Ipsum/Database/RZA-Data.db") as conn:
+        print("Connection Established")
+        forename = request.json.get("forename")
+        print(forename)
+        surname = request.json.get("surname")
+        dob = request.json.get("dob")
+        print(dob)
+        if not LF.DateCheck(dob):
+            return jsonify(
+                {
+                    "success": False,
+                    "message": "Value is either not a date or is in the incorrect format (DD/MM/YYYY)",
+                }
+            )
+        username = request.json.get("username")
+        print(username)
+        if LF.UsernameCheck(username):
+            return jsonify(
+                {
+                    "success": False,
+                    "message": "Username must be 5 - 16 characters longs and alphanumeric",
+                }
+            )
+        count = """Select Count(Username) From Users Where Username = ?"""
+        cu = conn.cursor()
+        print("Cursor Created")
+        cu.execute(count, (username,))
+        results = cu.fetchall()
+        for i in results:
+            if int(i[0]) > 1:
+                return jsonify({"success": False, "message": "Username is Taken"})
+        password = request.json.get("password")
+        if LF.PasswordCheck(password):
+            password = password.encode("utf-8")
+        else:
+            return jsonify(
+                {
+                    "success": False,
+                    "message": "Password must be between 5 and 16 characters and have atleast 1 uppercase character, 1 lowercase character and 1 number",
+                }
+            )
+        salt = gensalt()
+        hashedPassword = hashpw(password, salt)
+        try:
+            statement = """INSERT INTO Users(Forename, Surname, Username, DOB, Password, RoleID) VALUES(?,?,?,?,?,?)"""
+            CUSTOMER_ROLE = 0
+            cu.execute(
+                statement,
+                (forename, surname, username, dob, hashedPassword, CUSTOMER_ROLE),
+            )
+            conn.commit()
+            return jsonify({"success": True, "message": "Login Successful", "role": 0})
+        except Error as e:
+            print("Value could not be added to DB", e)
+            return jsonify({"success": False, "message": "Internal Server Error"})
+
+
+@app.route("/staffsignup", methods=["POST"])
+def StaffSignupDetails():
+    print("Request Recieved")
+    with sqlite3.connect(r"Lorem-Ipsum/Database/RZA-Data.db") as conn:
+        print("Connection Established")
+        email = request.json.get("email")
+        if not LF.EmailCheck(email):
+            return jsonify({"success": False, "message": "Not a Valid Email"})
+        username = request.json.get("username")
+        print(username)
+        if LF.UsernameCheck(username):
+            return jsonify(
+                {
+                    "success": False,
+                    "message": "Username must be 5 - 16 characters longs and alphanumeric",
+                }
+            )
+        count = """Select Count(Username) From Users Where Username = ?"""
+        cu = conn.cursor()
+        print("Cursor Created")
+        cu.execute(count, (username,))
+        results = cu.fetchall()
+        for i in results:
+            if int(i[0]) > 1:
+                return jsonify({"success": False, "message": "Username is Taken"})
+        password = request.json.get("password")
+        if LF.PasswordCheck(password):
+            password = password.encode("utf-8")
+        else:
+            return jsonify(
+                {
+                    "success": False,
+                    "message": "Password must be between 5 and 16 characters and have atleast 1 uppercase character, 1 lowercase character and 1 number",
+                }
+            )
+        salt = gensalt()
+        hashedPassword = hashpw(password, salt)
+        try:
+            statement = """INSERT INTO Users(Username, Password, Email) VALUES(?,?,?)"""
+            STAFF_DEFAULT_ROLE = 4
+            cu.execute(
+                statement,
+                (username, hashedPassword, email, STAFF_DEFAULT_ROLE),
+            )
+            conn.commit()
+            return jsonify(
+                {"success": True, "message": "Staff Account Created", "role": 0}
+            )
+        except Error as e:
+            print("Value could not be added to DB", e)
+            return jsonify({"success": False, "message": "Internal Server Error"})
+
+if __name__ == "__main__":
+    app.run()
+    # ssl_context="adhoc"
