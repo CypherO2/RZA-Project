cad00c156ca1741c2b9735076b478bc20b62539e
CJ-Presley <144433536+CJ-Presley@users.noreply.github.com>
2024-04-19 13:04:50 +0000

    payment portal template in place

--- FrontEnd/RZA-DigitalSolution/src/pages/Basket.tsx
+++ FrontEnd/RZA-DigitalSolution/src/pages/Basket.tsx
@@ -1,11 +1,114 @@
 import { Container } from "react-bootstrap";
 import AhAhAh from "../assets/AhAhAh.gif";
+import { useEffect } from "react";
+import {
+  MDBBtn,
+  MDBCard,
+  MDBCardBody,
+  MDBCol,
+  MDBContainer,
+  MDBIcon,
+  MDBInput,
+  MDBRadio,
+  MDBRow,
+} from "mdb-react-ui-kit";
 
 export default function Basket() {
+  useEffect(() => {
+    document.title = "Riget Zoo | Basket";
+  });
   return (
     <>
-      <Container className="py-5 my-5">
-        <h1 className="fw-bold">
+      <MDBContainer className="py-5" fluid>
+        <MDBRow className=" d-flex justify-content-center">
+          <MDBCol md="10" lg="8" xl="5">
+            <MDBCard className="rounded-3">
+              <MDBCardBody className="p-4">
+                <div className="text-center mb-4">
+                  <h3>Checkout</h3>
+                  <h6>Payment</h6>
+                </div>
+                <p className="fw-bold mb-4 pb-2">Saved cards:</p>
+                <div className="d-flex flex-row align-items-center mb-4 pb-1">
+                  <img
+                    className="img-fluid"
+                    src="https://img.icons8.com/color/48/000000/mastercard-logo.png"
+                  />
+                  <div className="flex-fill mx-3">
+                    <div className="form-outline">
+                      <MDBInput
+                        label="Card Number"
+                        id="form1"
+                        type="text"
+                        size="lg"
+                        value="**** **** **** 3193"
+                      />
+                    </div>
+                  </div>
+                  <a href="#!">Remove card</a>
+                </div>
+                <div className="d-flex flex-row align-items-center mb-4 pb-1">
+                  <img
+                    className="img-fluid"
+                    src="https://img.icons8.com/color/48/000000/visa.png"
+                  />
+                  <div className="flex-fill mx-3">
+                    <div className="form-outline">
+                      <MDBInput
+                        label="Card Number"
+                        id="form2"
+                        type="text"
+                        size="lg"
+                        value="**** **** **** 4296"
+                      />
+                    </div>
+                  </div>
+                  <a href="#!">Remove card</a>
+                </div>
+                <p className="fw-bold mb-4">Add new card:</p>
+                <MDBInput
+                  label="Cardholder's Name"
+                  id="form3"
+                  type="text"
+                  size="lg"
+                />
+                <MDBRow className="my-4">
+                  <MDBCol size="7">
+                    <MDBInput
+                      label="Card Number"
+                      id="form4"
+                      type="text"
+                      size="lg"
+                    />
+                  </MDBCol>
+                  <MDBCol size="3">
+                    <MDBInput
+                      label="Expire"
+                      id="form5"
+                      type="password"
+                      size="lg"
+                      placeholder="MM/YYYY"
+                    />
+                  </MDBCol>
+                  <MDBCol size="2">
+                    <MDBInput
+                      label="CVV"
+                      id="form6"
+                      type="password"
+                      size="lg"
+                      placeholder="CVV"
+                    />
+                  </MDBCol>
+                </MDBRow>
+                <MDBBtn color="success" size="lg" block>
+                  Add card
+                </MDBBtn>
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+      {/* <h1 className="fw-bold">
           This Page Exists | Due to the nature of this site we cannot accept
           Payments at this time.
         </h1>
@@ -22,8 +125,7 @@ export default function Basket() {
           src={AhAhAh}
           alt="This Page is Unavaliable Right Now!"
           style={{ height: "20rem" }}
-        />
-      </Container>
+        /> */}
     </>
   );
 }
