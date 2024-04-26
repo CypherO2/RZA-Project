617b39bb95fda46c9815b317b2aa0b46bc7c4b37
CJ-Presley <144433536+CJ-Presley@users.noreply.github.com>
2024-04-26 08:21:11 +0000

    deleted unneeded files

--- FrontEnd/RZA-DigitalSolution/src/pages/Dashboard.tsx
+++
@@ -1,9 +0,0 @@
-import { useEffect } from "react";
-import { Container } from "react-bootstrap";
-
-export default function DashboardPage() {
-  useEffect(() => {
-    document.title = "Riget Zoo | Dashboard";
-  });
-  return <><Container></Container></>;
-}
